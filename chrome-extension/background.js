bfac9bfa68cd4a5063f2b48a71f5af7f665d0abd
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-04-04 19:04:39 +0900

    feat: 拡張ボタンクリックでGoogle翻訳ページを開くChrome拡張を作成

---
+++ chrome-extension/background.js
@@ -0,0 +1,21 @@
+chrome.browserAction.onClicked.addListener(tab => {
+	if (tab.url.startsWith('chrome://')) return;
+	if (tab.url.startsWith('file://')) return;
+
+	const query = Object.entries({
+		/* 元の言語 */
+		sl: 'en',
+		/* 翻訳する言語 */
+		tl: 'ja',
+		u: tab.url,
+		/* 原文を表示する */
+		anno: 2,
+	}).map(([key, value]) => {
+		return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+	}).join('&');
+
+	chrome.tabs.create({
+		url: `https://translate.google.co.jp/translate?${query}`,
+		openerTabId: tab.id,
+	});
+});
